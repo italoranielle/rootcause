4349dd163d9a1077581c1daac92e58bc02af552c
italo ranielle <italoranielle@gmail.com>
2024-09-02 21:48:05 -0300

    debug variable

--- confiabilidade/settings.py
+++ confiabilidade/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"],
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG",True)
+DEBUG = False if os.environ.get("DEBUG",None) == 'False' else True
 
 ALLOWED_HOSTS = ['*']
 
